43913c53fdde37de4ed4f469bfbcffe8a31eaae0
chrisnotthere <mr.chrisnash@gmail.com>
2024-03-05 16:32:03 -0800

    server side validation

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -7,9 +7,11 @@ namespace BulkyWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)]
         [Display(Name = "Category Name")]
         public string Name { get; set; }
         [Display(Name = "Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100.")]
         public int DisplayOrder { get; set; }
     }
 }
